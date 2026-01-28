96412153dac899e0125b9d9ac849ff3bb09938e8
tomcomtang <364786053@qq.com>
2026-01-28 20:18:44 +0800

    Add EdgeOne Vite adapter: @edgeone/vite plugin and dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import vike from 'vike/plugin'
+import edgeone from '@edgeone/vite'
 
 export default defineConfig({
-  plugins: [react(), vike()]
+  plugins: [
+    react(), vike(), edgeone()]
 })
 
